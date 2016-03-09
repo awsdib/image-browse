ed9b474d6f3a6447fce97237f430658e6d30280f
David LeGare <david.legare@scientificgames.com>
2016-03-09 12:50:04 -0600

    Add Site Input To Home Page

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -3,4 +3,10 @@ import {Page} from 'ionic-angular';
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
-export class HomePage {}
+export class HomePage {
+  siteInput: string = '';
+
+  submit() {
+    console.log(`site address: ${this.siteInput}`);
+  }
+}
