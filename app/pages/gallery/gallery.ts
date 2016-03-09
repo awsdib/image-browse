a3434899c8e19a7bb6169604b9173fc3cc7ec321
David LeGare <david.legare@scientificgames.com>
2016-03-09 17:17:17 -0600

    Add Sample Data For safebooru.org

--- app/pages/gallery/gallery.ts
+++ app/pages/gallery/gallery.ts
@@ -1,13 +1,16 @@
 import {NavController, NavParams, Page} from 'ionic-angular';
-
+import {GelbooruService, Post} from '../../backends/gelbooru-service';
 
 @Page({
   templateUrl: 'build/pages/gallery/gallery.html'
 })
 export class GalleryPage {
   siteName: string;
+  posts: Array<Post>;
 
   constructor(private nav: NavController, navParams: NavParams) {
     this.siteName = navParams.get('siteName');
+    this.posts = (new GelbooruService(this.siteName)).getPosts();
+    console.log(this.posts);
   }
 }
