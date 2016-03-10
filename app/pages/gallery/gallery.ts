507bb21992db8d7cddc4b85c3461c3180991bee7
David LeGare <david.legare@scientificgames.com>
2016-03-10 16:48:22 -0600

    Use Mock Data On Desktop
    - Move safebooru mock data into separate file.
    - Use Platform to detect when we’re on desktop and only use mock data
    then.
    - Add Android as a platform.
    - Display an alert when getting posts fails.

--- app/pages/gallery/gallery.ts
+++ app/pages/gallery/gallery.ts
@@ -1,24 +1,44 @@
-import {NavController, NavParams, Page} from 'ionic-angular';
+import {Alert, NavController, NavParams, Page} from 'ionic-angular';
 import {ImagePage} from '../image/image';
 import {GelbooruService} from '../../backends/gelbooru-service';
 
 @Page({
-  templateUrl: 'build/pages/gallery/gallery.html'
+  templateUrl: 'build/pages/gallery/gallery.html',
+  providers: [GelbooruService],
 })
 export class GalleryPage {
   siteName: string;
   posts: Post[];
   rows: Post[][];
 
-  constructor(private nav: NavController, navParams: NavParams) {
+  constructor(
+    private nav: NavController,
+    private provider: GelbooruService,
+    navParams: NavParams
+  ) {
     this.siteName = navParams.get('siteName');
-    this.posts = (new GelbooruService(this.siteName)).getPosts();
+    this.provider.getPosts(
+      this.siteName,
+      0,
+      (posts) => {
+        this.posts = posts;
 
-    const POSTS_PER_ROW = 3;
-    this.rows = [];
-    for (let start = 0; start < this.posts.length; start += POSTS_PER_ROW) {
-      this.rows.push(this.posts.slice(start, start + POSTS_PER_ROW));
-    }
+        // Break posts into rows so that they can be displayed in a grid.
+        const POSTS_PER_ROW = 3;
+        this.rows = [];
+        for (let start = 0; start < this.posts.length; start += POSTS_PER_ROW) {
+          this.rows.push(this.posts.slice(start, start + POSTS_PER_ROW));
+        }
+      },
+      (message: string) => {
+        let alert = Alert.create({
+          title: 'Error Loading Posts',
+          subTitle: message,
+          buttons: ['Ok'],
+        });
+        this.nav.present(alert);
+      }
+    );
   }
 
   buttonClick(post: Post) {
