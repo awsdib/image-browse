eede044098c9e343b9355e3dd02344185205abfa
David LeGare <david.legare@scientificgames.com>
2016-04-15 10:24:07 -0500

    Update To Ionic 2 Beta 4

---
+++ typings/main.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="main/ambient/es6-shim/index.d.ts" />
+/// <reference path="main/ambient/object/index.d.ts" />
+/// <reference path="main/ambient/uri/index.d.ts" />
+/// <reference path="main/ambient/xml2js/index.d.ts" />
