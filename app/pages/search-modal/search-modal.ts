e1c8f294c7c91472bba6adb5f6d128f79a7f67e4
David LeGare <david.legare@scientificgames.com>
2016-04-26 13:29:13 -0500

    feat(search): add search modal
    - Add search button to gallery title bar to open search modal.
    - Implement editing current tags in search modal.
    - Implement cancel and search button from search modal.
    - Fix site title in search bar of gallery page.
    - Cleanup in home page.

---
+++ app/pages/search-modal/search-modal.ts
@@ -0,0 +1,53 @@
+import {Modal, NavController, NavParams, Page, ViewController} from 'ionic-angular';
+import * as gallery from '../gallery/gallery';
+import {Options} from '../../backends/gelbooru-service';
+
+@Page({
+  templateUrl: 'build/pages/search-modal/search-modal.html',
+})
+export class SearchModal {
+  hostname: string;
+  options: Options;
+
+  editTags: any[] = [];
+  editExcludeTags: any[] = [];
+
+  constructor(
+    private nav: NavController,
+    private viewController: ViewController,
+    private navParams: NavParams
+  ) {
+    this.hostname = navParams.get('hostname');
+    this.options = navParams.get('options');
+
+    if (this.options.tags != null)
+    {
+      for (let tag of this.options.tags) {
+        this.editTags.push({
+          text: tag,
+        });
+      }
+    }
+  }
+
+  onClose() {
+    this.viewController.dismiss();
+  }
+
+  onSearch() {
+    // Create a new Options from the edited tags.
+    let options = {
+      tags: this.editTags.map(tag => tag.text),
+      excludeTags: this.options.excludeTags,
+    };
+
+    console.log("options: ");
+    console.log(options);
+
+    // TODO: Validate url input before sending it to gallery page.
+    this.nav.push(gallery.GalleryPage, {
+      'hostname': this.hostname,
+      'options': options,
+    });
+  }
+}
